e86c523f4ead8f0cfbc7d359ca31d33510ee7010
Alexander Bogachenkov <aleksbogac@gmail.com>
2024-05-30 16:16:13 +0300

    Added Http error handling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,28 +1,37 @@
 import {Component, OnInit} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import {NgForOf, TitleCasePipe} from "@angular/common";
+import {AsyncPipe, NgForOf, NgIf, TitleCasePipe} from "@angular/common";
 import { ProductComponent } from './components/product/product.component';
 import {IProduct} from "./models/product";
 import {ProductsService} from "./services/products.service";
+import {Observable, tap} from "rxjs";
+import {GlobalErrorComponent} from "./components/global-error/global-error.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, TitleCasePipe, ProductComponent, NgForOf],
+  imports: [RouterOutlet, TitleCasePipe, ProductComponent, NgForOf, NgIf, AsyncPipe, GlobalErrorComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 
 export class AppComponent implements OnInit {
   title = 'First Angular App'
-  products: IProduct[] = []
+  // products: IProduct[] = []
+  loading = false
+  products$: Observable<IProduct[]>
 
   constructor(private productsService: ProductsService) {
   }
 
   ngOnInit(): void {
-    this.productsService.getAll().subscribe(products => {
-      this.products = products
-    })
+    this.loading = true
+    this.products$ = this.productsService.getAll().pipe(
+      tap(() => this.loading = false)
+    )
+    // this.productsService.getAll().subscribe(products => {
+    //   this.products = products
+    //   this.loading = false
+    // })
   }
 }
