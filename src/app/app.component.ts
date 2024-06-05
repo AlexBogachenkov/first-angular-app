51aa984262ca069cfc3205a08be147f5b6596519
Alexander <aleksbogac@gmail.com>
2024-06-05 23:19:51 +0300

    Added navigation bar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,32 +11,16 @@ import {FilterProductsPipe} from "./pipes/filter-products.pipe";
 import {ModalComponent} from "./components/modal/modal.component";
 import {CreateProductComponent} from "./components/create-product/create-product.component";
 import {ModalService} from "./services/modal.service";
+import {NavigationComponent} from "./components/navigation/navigation.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, TitleCasePipe, ProductComponent, NgForOf, NgIf, AsyncPipe, GlobalErrorComponent, FormsModule, FilterProductsPipe, ModalComponent, CreateProductComponent],
+  imports: [RouterOutlet, TitleCasePipe, ProductComponent, NgForOf, NgIf, AsyncPipe, GlobalErrorComponent, FormsModule, FilterProductsPipe, ModalComponent, CreateProductComponent, NavigationComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 
-export class AppComponent implements OnInit {
-  title = 'First Angular App'
-  products: IProduct[] = []
-  loading = false
-  // products$: Observable<IProduct[]>
-  term = ''
+export class AppComponent {
 
-  constructor(public productsService: ProductsService, public modalService: ModalService) {
-  }
-
-  ngOnInit(): void {
-    this.loading = true
-    // this.products$ = this.productsService.getAll().pipe(
-    //   tap(() => this.loading = false)
-    // )
-    this.productsService.getAll().subscribe(() => {
-      this.loading = false
-    })
-  }
 }
--- src/app/pages/prooduct-page/product-page.component.ts
+++ src/app/pages/prooduct-page/product-page.component.ts
@@ -1,12 +1,49 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {AsyncPipe, NgForOf, NgIf, TitleCasePipe} from "@angular/common";
+import {CreateProductComponent} from "../../components/create-product/create-product.component";
+import {FilterProductsPipe} from "../../pipes/filter-products.pipe";
+import {ModalComponent} from "../../components/modal/modal.component";
+import {ProductComponent} from "../../components/product/product.component";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {IProduct} from "../../models/product";
+import {ProductsService} from "../../services/products.service";
+import {ModalService} from "../../services/modal.service";
 
 @Component({
   selector: 'app-prooduct-page',
   standalone: true,
-  imports: [],
+  imports: [
+    AsyncPipe,
+    CreateProductComponent,
+    FilterProductsPipe,
+    ModalComponent,
+    NgForOf,
+    NgIf,
+    ProductComponent,
+    ReactiveFormsModule,
+    TitleCasePipe,
+    FormsModule
+  ],
   templateUrl: './product-page.component.html',
   styleUrl: './product-page.component.scss'
 })
-export class ProductPageComponent {
+export class ProductPageComponent implements OnInit {
+  title = 'First Angular App'
+  products: IProduct[] = []
+  loading = false
+  // products$: Observable<IProduct[]>
+  term = ''
 
+  constructor(public productsService: ProductsService, public modalService: ModalService) {
+  }
+
+  ngOnInit(): void {
+    this.loading = true
+    // this.products$ = this.productsService.getAll().pipe(
+    //   tap(() => this.loading = false)
+    // )
+    this.productsService.getAll().subscribe(() => {
+      this.loading = false
+    })
+  }
 }
